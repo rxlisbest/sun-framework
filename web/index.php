7f7dc1c129bd02f8d065c9bd04ab38f993105c24
roy <rxlisbest@163.com>
2018-01-16 17:08:58 +0800

    修改入口文件

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 require '../vendor/autoload.php';
-$test = new Rxlisbest\Sun\Core\Test();
-$test->sayHello();
-var_dump(3);exit;
-var_dump('start');
+
+$application = new Rxlisbest\Sun\Application();
+$application->run();
