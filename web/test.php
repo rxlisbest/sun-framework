663c5188b4fd96835ec4131f430f18167fa79852
Roy <rxlisbest@gmail.com>
2018-10-14 21:03:14 +0800

    test

--- web/test.php
+++ web/test.php
@@ -1,8 +1,8 @@
 <?php
 define('SUN_DEBUG', true);
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$config = require __DIR__ . '/../config/config.php';
+$config = require __DIR__ . '/../config/test.php';
 
 $application = new Rxlisbest\Sun\Application($config);
 $application->run();
