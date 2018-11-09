a7fa942825feb5cdcaad7b7a08c5d61c0abba46c
Roy <rxlisbest@gmail.com>
2018-11-09 09:44:43 +0800

    修改忽略文件

--- config/console.php
+++ config/console.php
@@ -1,12 +1,11 @@
 <?php
+/**
+ *
+ */
+$database = require(__DIR__ . '/database.php');
+
 return [
     'base_path' => dirname(__DIR__),
     'path_info' => true,
-    'database' => [
-        'dsn' => 'mysql:host=localhost;dbname=sun_test',
-        'username' => 'root',
-        'password' => 'root',
-        'charset' => 'utf8',
-        'prefix' => 'test_'
-    ]
+    'database' => $database
 ];
