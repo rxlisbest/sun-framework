65fd991314c1bdc93f57491a9f439a4826ef2f64
Roy <rxlisbest@gmail.com>
2019-11-18 17:14:42 +0800

    .env

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
+  <meta charset="UTF-8">
+  <title>Welcome</title>
 </head>
 <body>
-    <h1>Welcome to use <?php echo $name?>!</h1>
+<h1>Welcome to use <?= $name ?>!</h1>
 </body>
 </html>
--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 return [
     'dsn' => 'mysql:host=localhost;dbname=taobao_2',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'test',
+    'password' => 'test',
     'charset' => 'utf8',
     'prefix' => 'test_'
 ];
